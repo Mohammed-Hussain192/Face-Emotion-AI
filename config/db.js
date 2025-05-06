559569822134ef227bf7a9e8c1f093915213f340
Mohammed-Hussain192 <Mohammedhussain7846@gmail.com>
2025-05-06 14:40:48 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/Emotion-Ai', {
+ 
+})
+.then(() => console.log('✅ MongoDB connected successfully!'))
+.catch(err => console.error('❌ MongoDB connection error:', err));
---
+++ controllers/push.js
@@ -0,0 +1,6 @@
+
+const monogoose = require('mongoose')
+const push = ( req,res,username,email,password, )=>{
+    console.log(username,email,password)
+} 
+module.exports=push
